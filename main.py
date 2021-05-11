af86e3df53810fb03e4a0e3e50f8685612227b16
yuu-eguci <yuu.eguci@gmail.com>
2021-05-11 15:12:58 +0900

    #1 ✨ コマンドライン引数 slack-notification

--- main.py
+++ main.py
@@ -18,16 +18,21 @@ logger.info(f'Started at {current_utc.isoformat()}')
 current_jst = datetime.datetime.now(tz=pytz.timezone('Asia/Tokyo'))
 logger.info(f'Started at {current_jst.isoformat()}')
 
-# head branch と base branch を引数から取得します。
+# コマンドライン引数を取得します。
 # head -> base の PR となります。
 # NOTE: TOKEN 等は環境変数で良いですが、ブランチは実行ごとに可変にしたい。
+# NOTE: release への PR は通知したいが、 early-access への PR は必要ない。
+#       slack_notification 引数はその分岐を行うために追加しました。
 parser = argparse.ArgumentParser()
 parser.add_argument('-H', '--head', type=str, help='head -> base',
                     default='dev')
 parser.add_argument('-B', '--base', type=str, help='head -> base',
                     default='master')
+parser.add_argument('-S', '--slack-notification', type=bool,
+                    help='Send release note message to Slack', default=False)
 args = parser.parse_args()
-logger.info(f'PR [{args.head} -> {args.base}] will be made.')
+logger.info(f'PR [{args.head} -> {args.base}] will be made.'
+            f' Slack notification is {"on" if args.slack_notification else "off"}.')
 
 # api を使って PR を作ります。
 create_pull_result = functions.create_pull(args.head, args.base)
